a55b63ca7bbf365ba37593ac65bb299aec689e00
Vagner Rodrigues <vagnux@gmail.com>
2021-02-10 15:08:31 -0300

    start backend

---
+++ logmatricsBackend/src/main/java/com/logmatrics/backend/LogmatricsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.logmatrics.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogmatricsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogmatricsBackendApplication.class, args);
+	}
+
+}
